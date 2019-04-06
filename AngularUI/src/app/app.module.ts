18e2ebbc0d69f25dfae8328e9dd2ff67a64267c4
Ailen <ailensarmukhanova@gmail.com>
2019-04-06 17:56:02 -0500

    Added bootstrap

--- AngularUI/src/app/app.module.ts
+++ AngularUI/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { BooksComponent } from './books/books.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { CategoriesComponent } from './categories/categories.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +24,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
     BooksComponent,
     CheckoutComponent,
     MyOrdersComponent,
+    CategoriesComponent,
   ],
   imports: [
     HttpClientModule,
