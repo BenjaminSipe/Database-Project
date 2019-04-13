e4b8d293a1be973d3e7a9d16b19b55bc0074c818
Benjamin Max Sipe <sipe.nation3@gmail.com>
2019-04-13 18:52:22 -0500

    Working Data Retrieval Using Get

--- AngularUI/src/app/user.ts
+++ AngularUI/src/app/user.ts
@@ -1,10 +1,10 @@
 export class User {
   public userID:number;
   public email:string;
+  public name:string;
   public password:string;
   public homePhone:string;
   public workPhone:string;
-  public name:string;
   
   constructor(email:string, 
     password:string
