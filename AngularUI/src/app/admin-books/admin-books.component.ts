1ec70706ca43444fb04737029df91b06b02287cb
Ailen <ailensarmukhanova@gmail.com>
2019-04-16 14:37:49 -0500

    Added book list.

--- AngularUI/src/app/admin-books/admin-books.component.ts
+++ AngularUI/src/app/admin-books/admin-books.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { GETService } from '../services/get.service';
 
 @Component({
-  selector: 'app-admin-books',
+  selector: 'admin-books',
   templateUrl: './admin-books.component.html',
   styleUrls: ['./admin-books.component.sass']
 })
-export class AdminBooksComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class AdminBooksComponent {
+  books$;
+  constructor(private getService : GETService) {
+    this.books$ = this.getService.getBooks();
+   }
 
 }
