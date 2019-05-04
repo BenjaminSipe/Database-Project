f62694267fc9a518eee932ff3d8b1fe5019f4718
Benjamin Max Sipe <sipe.nation3@gmail.com>
2019-05-04 16:03:51 -0500

    Here you go.

--- AngularUI/src/app/confirmation-page/confirmation-page.component.ts
+++ AngularUI/src/app/confirmation-page/confirmation-page.component.ts
@@ -1,32 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { GETService } from '../services/get.service';
 import { UserService } from '../services/user.service';
 import { User } from '../user';
 import { Router } from '@angular/router';
-=======
-import { UserService } from '../services/user.service';
->>>>>>> fd9033ae3ff2d0fa82860a9667711c2b7d9281af
 
 @Component({
   selector: 'app-confirmation-page',
   templateUrl: './confirmation-page.component.html',
   styleUrls: ['./confirmation-page.component.sass']
 })
-<<<<<<< HEAD
 export class ConfirmationPageComponent{
   firstName;
   user = new User();
   constructor(private userservice: UserService, private get: GETService,
               private router: Router) {
-    console.log(userservice.user.firstName);
-=======
-export class ConfirmationPageComponent implements OnInit {
-
-  constructor(private userservice:UserService) { }
-
-  ngOnInit() {
->>>>>>> fd9033ae3ff2d0fa82860a9667711c2b7d9281af
   }
 
 }
